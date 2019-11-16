d3d38d3578dfd947579d2cafc6ef9dd6ccd89be8
Frank Selhorst <info@frankselhorst.nl>
2019-11-16 01:07:02 +0100

    Update test.js
    changed products to mock

--- tests/test.js
+++ tests/test.js
@@ -30,7 +30,7 @@ describe('pathfindr tests', () => {
     })
 
     it('should traverse correctly with array as root', () => {
-        const products = [
+        const mock = [
             {
                 title: 'Soup',
                 price: 10,
@@ -41,34 +41,35 @@ describe('pathfindr tests', () => {
                 }
             }
         ]
-        const value = pathfindr('0/data/images/0/url', products);
+        const value = pathfindr('0/data/images/0/url', mock);
         expect(value).toBe('/soup.png');
     })
 
     it('should not throw error when null or undefined encountered', () => {
-        const products = [
+        const mock = [
             {
                 title: 'Soup',
                 price: 10,
             }
         ];
-        const value = pathfindr('0/data/images/0/url', products, 'fallback.png');
+        const value = pathfindr('0/data/images/0/url', mock, 'fallback.png');
         expect(value).toBe('fallback.png');
     })
     it('should not throw error when model is undefined or null', () => {
         expect.assertions(2)
-        const productsNull = null;
-        const productsUndefined = undefined;
-        const value1 = pathfindr('0/data/images/0/url', productsNull);
-        const value2 = pathfindr('0/data/images/0/url', productsUndefined);
+        const mockNull = null;
+        const mockUndefined = undefined;
+        
+        const value1 = pathfindr('0/data/images/0/url', mockNull);
+        const value2 = pathfindr('0/data/images/0/url', mockUndefined);
         expect(value1).toBe(null);
         expect(value2).toBe(null);
     })
 
     it('should set fallback value when undefind or null encountered', () => {
         expect.assertions(1)
-        const products = null;
-        const value = pathfindr('0/data/images/0/url', products, 'fallback');
+        const mock = null;
+        const value = pathfindr('0/data/images/0/url', mock, 'fallback');
         expect(value).toBe('fallback');
     })
 
